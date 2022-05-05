1c56be92449b4c3a7495d87ce1c7fe169c7a8ba5
ZOG-WEB <79850764+ZOG-WEB@users.noreply.github.com>
2022-05-05 17:45:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas=createCanvas(550,500);
+canvas.position(100,250);
+video=createCapture(VIDEO);
+video.hide();
+tint_color="";
+}
+
+function draw(){
+    image(video,0,0,550,500);
+    tint(tint_color);
+}
+
+function take_snapshot(){
+    save('image.png');
+
+}
+
+function filter_tint(){
+    tint_color=document.getElementById("colorname").value;
+}
+
+
